de6a16c4aedc5c8344a98c36d83ce3af8492b797
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2016-10-12 16:58:00 +0300

    Completed implementing half of last assignment.

---
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -0,0 +1,25 @@
+package calculator
+
+object Polynomial {
+  def computeDelta(a: Signal[Double], b: Signal[Double],
+                   c: Signal[Double]): Signal[Double] = {
+    Signal(b() * b() - 4 * a() * c())
+  }
+
+  def computeSolutions(a: Signal[Double], b: Signal[Double],
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = Signal {
+    val aVal = a()
+    val bVal = b()
+    val deltaVal = delta()
+    c()
+
+    if (deltaVal < 0.0) Set.empty
+    else if (deltaVal == 0.0) Set(-bVal / (2.0 * aVal))
+    else {
+      import math.sqrt
+      val x1 = (-bVal - sqrt(deltaVal)) / (2.0 * aVal)
+      val x2 = (-bVal + sqrt(deltaVal)) / (2.0 * aVal)
+      Set(x1, x2)
+    }
+  }
+}
